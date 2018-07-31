48e9653cc547cb6a694e31a67e30d4c51b50c8b0
ikhsan kurniawan <kurniawanikhsan97@gmail.com>
2018-07-31 23:56:03 +0700

    310-07-18
    coba bikin dadu

---
+++ ludo kimia/Assets/Script/diceRoll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class diceRoll : MonoBehaviour {
+
+	public Text hasilRandom;
+	public string nilaiDadu;
+	public Sprite[] dice;
+
+
+	System.Random randomDice = new System.Random();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RollDice(){
+		int randNum = randomDice.Next (1, 7);
+		nilaiDadu = randNum.ToString();
+		hasilRandom.text = nilaiDadu;
+
+	}
+}
